7be7cf5b88c02cfc7726e973e5ab20070db7946d
Justin Li <jli.justinli@gmail.com>
2012-05-03 22:38:56 -0400

    Moved example to its own directory and added npmignore

---
+++ examples/stdio.js
@@ -0,0 +1,50 @@
+/*!
+ * node-rcon - examples/stdio.js
+ * Copyright(c) 2012 Justin Li <j-li.net>
+ * MIT Licensed
+ */
+
+var Rcon = require('rcon');
+
+var conn = new Rcon('localhost', 1234, 'password');
+conn.on('auth', function() {
+
+  console.log("Authed!");
+
+}).on('response', function(str) {
+
+  console.log("Got response: " + str);
+
+}).on('end', function() {
+
+  console.log("Socket closed!");
+  process.exit();
+
+});
+
+conn.connect();
+
+
+/*
+ * example reads commands from stdin and sends them on enter key press
+ */
+
+var stdin = process.openStdin(); 
+require('tty').setRawMode(true);    
+
+var buffer = "";
+
+stdin.on('keypress', function (chunk, key) {
+  if (key && key.ctrl && key.name == 'c') {
+    conn.disconnect();
+    return;
+  }
+  if (key && key.name == 'enter') {
+    conn.send(buffer);
+    buffer = "";
+  } else {
+    buffer += chunk;
+  }
+  process.stdout.write(chunk);
+
+});
